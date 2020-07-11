fcb6d788817c194376f2769758a09b8d8243cf5e
ramabna <66891940+ramabna@users.noreply.github.com>
2020-07-12 02:05:36 +0700

    Update function.php

--- function.php
+++ function.php
@@ -17,8 +17,7 @@ function request(
     $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
     $header[] = "Connection: keep-alive";
     $header[] = "X-User-Locale: id_ID";
-    $header[] = "X-Location: -7.58".mt_rand(1000,9999).";
-                             -3.59".mt_rand(1000,9999).";
+    $header[] = "X-Location: -7.58".mt_rand(1000,9999)."; -3.59".mt_rand(1000,9999).";
     $header[] = "X-Location-Accuracy: 3.0";
 
     if($pin) {
